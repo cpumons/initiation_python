1fc4d6b200b6968617dddd369cc2609eb9760f50
arnaudPalgen <arnaud.palgen@student.umons.ac.be>
2020-02-10 11:37:41 +0100

    cputurtle OK !

--- code/cputurtle.py
+++ code/cputurtle.py
@@ -4,7 +4,16 @@ from PIL import Image
 import os
 import random
 
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
 t = turtle.Turtle()
+t.shape("turtle")
+t.color('green')
 
 
 def attend(msg="Press enter to quit."):
@@ -47,14 +56,45 @@ def couleur_aleatoire():
     t.pencolor((r, g, b))
 
 
-def save(email=None, filename="image.png"):
-    canvas = turtle.getScreen().getCanvas()
+def sauver(email=None, filename="image.png", envoyer=False):
+    canvas = t.getscreen().getcanvas()
     ps = canvas.postscript(colormode="color")
     img = Image.open(io.BytesIO(ps.encode("utf-8")))
     path = os.getcwd()
     img.save(os.path.join(path, filename))
     # todo send mail
 
+    if not envoyer:
+        return
+    
+    fromaddr = "adresse email" # a completer
+    toaddr = email
+
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Ton dessin réalisé à l'atelier de CPUMons !"
+    
+    body="Salut !\n Tu trouvera en pièce jointe le dessin que tu as réalisé avec notre petite tortue Turtle !\n\n A bientôt ! La team CPUMons\n "
+    msg.attach(MIMEText(body, 'plain'))
+    
+    attachment=open(filename, "rb")
+    p=MIMEBase('application', 'octet-stream')
+    p.set_payload((attachment).read())
+    encoders.encode_base64(p) 
+    p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    msg.attach(p)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587) 
+    s.starttls()
+    s.login(fromaddr, "mot de passe") # a completer
+    text = msg.as_string()
+    s.sendmail(fromaddr, toaddr, text)
+    s.quit() 
+
+
+    
 
 if __name__ == "__main__":
     epaisseur(3)
